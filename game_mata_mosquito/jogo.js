a59b9cd2c6be719f3c929fd9a9c85f464fd1d3b1
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-04-06 16:44:32 -0400

    inicio do jogo
    preparativos iniciais

---
+++ game_mata_mosquito/jogo.js
@@ -0,0 +1,14 @@
+var altura = 0
+var largura = 0
+
+function ajustaTela(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+}
+
+ajustaTela()
+
+function posicaoMosquito(){
+ var posicaoX = Math.random()
+
+}
