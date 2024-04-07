5bfca5f433b689237f22b221ab66b9c86944cc20
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-04-07 11:30:09 -0400

    mosquito randomico
    Incluido lógica para mosquito aparece em váios lugares na tela.

--- game_mata_mosquito/jogo.js
+++ game_mata_mosquito/jogo.js
@@ -9,6 +9,20 @@ function ajustaTela(){
 ajustaTela()
 
 function posicaoMosquito(){
- var posicaoX = Math.random()
+ var posicaoX = Math.floor(Math.random()* largura) -90
+ var posicaoY = Math.floor(Math.random() * altura) - 90
+ console.log(posicaoX, posicaoY)
 
+ posicaoX = posicaoX  < 0 ? 0: posicaoX 
+ posicaoY = posicaoY < 0  ? 0 : posicaoY
+
+  var mosquito = document.createElement('img')
+  mosquito.src ='imagens/imagens/mosquito.png'
+  mosquito.className ='mosquito'
+  mosquito.style.left = posicaoX+'px'
+  mosquito.style.top = posicaoY+'px'
+  mosquito.style.position ='absolute'
+
+  document.body.appendChild(mosquito)
 }
+posicaoMosquito()
