5f0cc56288cd5301f18513efec6df4e812cd92e0
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-04-14 23:31:51 -0400

    Criação dos Mosquitos, Tela de Game Over
    Foi implementado a criação dos mosquitos e desenvolvido a tela de Game over utilizando bootstrap

--- game_mata_mosquito/jogo.js
+++ game_mata_mosquito/jogo.js
@@ -1,28 +1,73 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTela(){
     altura = window.innerHeight
     largura = window.innerWidth
 }
 
-ajustaTela()
 
 function posicaoMosquito(){
+
+  //removendo o mosquito
+  if(document.getElementById('mosquito')){
+      document.getElementById('mosquito').remove()
+
+      if(vidas > 3){
+         window.location.href="game_over.html"
+      }
+      else{
+        document.getElementById('v'+vidas).src='imagens/coracao_vazio.png'
+        vidas++
+      }
+  }
  var posicaoX = Math.floor(Math.random()* largura) -90
  var posicaoY = Math.floor(Math.random() * altura) - 90
- console.log(posicaoX, posicaoY)
+ 
 
  posicaoX = posicaoX  < 0 ? 0: posicaoX 
  posicaoY = posicaoY < 0  ? 0 : posicaoY
 
   var mosquito = document.createElement('img')
-  mosquito.src ='imagens/imagens/mosquito.png'
-  mosquito.className ='mosquito'
+  mosquito.src ='imagens/mosquito.png'
+  mosquito.className = tamanhMosquito() +' '+ ladoAleatorio()
   mosquito.style.left = posicaoX+'px'
   mosquito.style.top = posicaoY+'px'
   mosquito.style.position ='absolute'
+  mosquito.id = 'mosquito'
+  mosquito.onclick = function(){
+    this.remove()
+  }
 
   document.body.appendChild(mosquito)
 }
-posicaoMosquito()
+
+function tamanhMosquito(){
+  var classe = Math.floor(Math.random()*3)
+
+  switch(classe){
+    case 0:
+      return 'mosquito1'
+    case 1:
+      return 'mosquito2'
+    case 2:
+      return 'mosquito3'
+  }
+}
+
+function ladoAleatorio(){
+  var classe =  Math.floor(Math.random()*2)
+  console.log(classe)
+  switch(classe){
+      case 0:
+        return 'ladoA'
+      case 1:
+        return 'ladoB'
+  }
+}
+
+ajustaTela()
+
+//criando um mosquito a cada um segundo
+setInterval(posicaoMosquito,1000)
